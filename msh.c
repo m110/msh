8a5a53fb9d3e14adf77a3968cfb6182388ef8e69
Miłosz Smółka <m110@m110.pl>
2016-12-17 12:15:29 +0100

    Add more basic functions

--- msh.c
+++ msh.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define MSH_READLINE_BUFSIZE 1024
+#define MSH_TOK_BUFSIZE 64
+#define MSH_TOK_DELIM "\t\r\a\n"
 
 void msh_fail(char *message) {
     fprintf(stderr, "msh: %s\n", message);
@@ -10,10 +16,10 @@ void msh_fail(char *message) {
 
 char *msh_read_line() {
     int bufsize = MSH_READLINE_BUFSIZE;
-    char *buffer = malloc(sizeof(char) * bufsize);
+    char *buffer = calloc(bufsize, sizeof(char));
 
     if (!buffer) {
-        msh_fail("buffer malloc error");
+        msh_fail("buffer calloc error");
     }
 
     int position = 0;
@@ -39,6 +45,54 @@ char *msh_read_line() {
     }
 
     return buffer;
+}
+
+char **msh_split_line(char *line) {
+    int bufsize = MSH_TOK_BUFSIZE;
+    char **tokens = calloc(bufsize, sizeof(char*));
+    if (!tokens) {
+        msh_fail("buffer calloc error");
+    }
+
+    int position = 0;
+    char *token = strtok(line, MSH_TOK_DELIM);
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += MSH_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+            if (!tokens) {
+                msh_fail("buffer realloc error");
+            }
+        }
+
+        token = strtok(NULL, MSH_TOK_DELIM);
+    }
+
+    tokens[position] = NULL;
+    return tokens;
+}
+
+int msh_launch(char **args) {
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        if (execvp(args[0], args) == -1) {
+            perror("msh");
+        }
+        exit(1);
+    } else if (pid < 0) {
+        perror("msh");
+    } else {
+        int status;
+        do {
+            pid_t wpid = waitpid(pid, &status, WUNTRACED);
+        } while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
 }
 
 void msh_load_config() {}
